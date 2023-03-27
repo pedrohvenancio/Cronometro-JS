34b374b630d69dc4dccf4eb5d4042100d357906d
Pedro Venâncio <ph.venancio2@gmail.com>
2023-03-27 14:24:16 -0300

    PC view, complete

---
+++ index.js
@@ -0,0 +1,48 @@
+//Variaveis
+const container = document.querySelector('.container');
+const iniciar = document.getElementById('iniciar');
+const parar = document.getElementById('parar');
+const resetar = document.getElementById('resetar');
+const hora = document.getElementById('horas');
+const minuto = document.getElementById('minutos');
+const segundo = document.getElementById('segundos');
+
+let h =0;
+let m =0;
+let s =0;
+
+// Iniciar
+function Iniciar(){
+    s++;
+    segundo.innerHTML = s;
+    if (s / 60 === 1){
+        s = 0;
+        m++;
+        minuto.innerHTML = m;
+        if (m / 60 === 1){
+            m = 0;
+            minuto.innerHTML = m;
+            h++;
+            hora.innerHTML = h;
+        }
+    }
+};
+
+var setIniciar;
+
+function timer(){
+    setIniciar = setInterval(Iniciar, 100);
+}
+iniciar.addEventListener('click', timer);
+
+// Parar
+const setParar = function() {clearInterval(setIniciar)};
+parar.addEventListener('click', setParar);
+
+// Resetar
+resetar.addEventListener('click', () => {
+        [h,m,s] = [0,0,0];
+        hora.innerHTML = "00";
+        minuto.innerHTML = "00";
+        segundo.innerHTML = "00";
+    });
